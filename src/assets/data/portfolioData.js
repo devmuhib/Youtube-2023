af0a97a84f95c0223b55a56cad8699a8cd6c409e
Muhibur Rahman <Muhib5532@gmail.com>
2023-03-18 09:02:26 +0600

    startup file

---
+++ src/assets/data/portfolioData.js
@@ -0,0 +1,153 @@
+import portfolioImg01 from "../images/portfolio-01.jpg";
+import portfolioImg02 from "../images/portfolio-02.jpg";
+import portfolioImg03 from "../images/portfolio-03.jpg";
+import portfolioImg04 from "../images/portfolio-04.jpg";
+import portfolioImg05 from "../images/portfolio-05.jpg";
+import portfolioImg06 from "../images/portfolio-06.jpg";
+import portfolioImg07 from "../images/portfolio-07.jpg";
+
+const portfolios = [
+  {
+    id: "01",
+    imgUrl: portfolioImg01,
+    category: "Web Design",
+    title: "Finance Technology Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "02",
+    imgUrl: portfolioImg04,
+    category: "Ux",
+    title: "Video Conference Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "03",
+    imgUrl: portfolioImg02,
+    category: "Ux",
+    title: "Multimedia Sharing Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "04",
+    imgUrl: portfolioImg05,
+    category: "Web Design",
+    title: "Landing Page",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "05",
+    imgUrl: portfolioImg03,
+    category: "Web Design",
+    title: "Landing Page",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+
+  {
+    id: "06",
+    imgUrl: portfolioImg06,
+    category: "Ux",
+    title: "Online Therapy Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "07",
+    imgUrl: portfolioImg07,
+    category: "Web Design",
+    title: "Appointment Booking Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "08",
+    imgUrl: portfolioImg01,
+    category: "Web Design",
+    title: "Finance Technology Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "09",
+    imgUrl: portfolioImg02,
+    category: "Ux",
+    title: "Video Conference Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "10",
+    imgUrl: portfolioImg03,
+    category: "Ux",
+    title: "File Sharing Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "11",
+    imgUrl: portfolioImg04,
+    category: "Web Design",
+    title: "Landing Page",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "12",
+    imgUrl: portfolioImg05,
+    category: "Web Design",
+    title: "Landing Page",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "13",
+    imgUrl: portfolioImg06,
+    category: "Web Design",
+    title: "Online Therapy Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "14",
+    imgUrl: portfolioImg07,
+    category: "ux",
+    title: "Appointment Booking Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+];
+
+export default portfolios;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#8873ef",
+        headingColor: "#081e21",
+        smallTextColor: "#193256",
+      },
+    },
+  },
+  plugins: [],
+};
